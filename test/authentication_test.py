b84f929b49924e2586e5c4150ed6c5055e46bbe1
Maxim Sokolov <maxim.sokolov@gmail.com>
2025-01-21 18:16:48 +0300

    Fix tests

--- test/authentication_test.py
+++ test/authentication_test.py
@@ -4,26 +4,22 @@ import os
 cap = Cap()
 
 def test_empty_credentials():
-    assert(cap._authenticate() == False)
-    assert(cap.get_error_status() == "Missing CAP client authetication settings. Check CAP_LOGIN, CAP_PWD or CAP_TOKEN enviroment variables.")
+    assert cap._authenticate() is False
+    assert cap.get_error_status() == "Missing CAP client authetication settings. Check CAP_LOGIN, CAP_PWD or CAP_TOKEN enviroment variables."
 
 def test_authentication_by_user():
     login = os.environ.get('CAP_LOGIN')
     pwd = os.environ.get('CAP_PWD')
     if login is None or pwd is None:
-        assert(cap._authenticate() == False)
+        assert cap._authenticate() is False
     else:
         # considering right credentials
-        assert(cap._authenticate() == True)
+        assert cap._authenticate() is True
 
 def test_authentication_by_token():
     token = os.environ.get('CAP_TOKEN')
     if token is None:
-        assert(cap._authenticate() == False)
+        assert cap._authenticate() is False
     else:
         # considering correct token
-        assert(cap._authenticate() == True)
-
-test_empty_credentials()    
-test_authentication_by_user()
-test_authentication_by_token()
+        assert cap._authenticate() is True
--- test/public_api_test.py
+++ test/public_api_test.py
@@ -10,21 +10,17 @@ def test_search_datasets():
         "body of stomach"
         ], sort=[{"name":"ASC"}])
     json_result = json.loads(response)
-    assert(json_result["results"])
+    assert json_result["results"] is not None
 
 
 def test_download_urls():
     response = cap.download_urls(678)
     json_result = json.loads(response)
-    assert(json_result["download_urls"])
+    assert json_result["download_urls"] is not None
 
 def test_search_cells():
     response = cap.search_cell_labels(search="blood", organism=["Homo sapiens"], tissue=[
         "parietal cortex",
         ], sort=[{"name":"ASC"}])
     json_result = json.loads(response)
-    assert(json_result["lookup_cells"])
-
-test_search_datasets()
-test_download_urls()
-test_search_cells()
+    assert json_result["lookup_cells"] is not None
