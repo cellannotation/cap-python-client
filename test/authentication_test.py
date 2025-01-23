86e205581f92ce5e47ff7c41f7515db16ff8feaa
Maxim Sokolov <maxim.sokolov@gmail.com>
2025-01-23 15:00:54 +0300

    Unit test fix

--- test/authentication_test.py
+++ test/authentication_test.py
@@ -7,7 +7,7 @@ def test_authentication_by_right_credentials_user():
     login = os.environ.get('CAP_LOGIN')
     pwd = os.environ.get('CAP_PWD')
     if login is None or pwd is None:
-        assert False, "Either CAP_LOGIN or CAP_PWD or both env variables are empty."
+        assert "Either CAP_LOGIN or CAP_PWD or both env variables are empty."
     else:
         # considering right credentials
         assert cap._authenticate() is True
@@ -19,7 +19,7 @@ def test_authentication_by_right_token():
     cap = Cap()
     token = os.environ.get('CAP_TOKEN')
     if token is None:
-        assert False, "CAP_TOKEN env variable is empty."
+        assert "CAP_TOKEN env variable is empty."
     else:
         # considering correct token
         assert cap._authenticate() is True
