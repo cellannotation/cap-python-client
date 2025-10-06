88004f10b46dde9b705f9d9d706c4b45b1c2e2b5
rm1113 <59999203+rm1113@users.noreply.github.com>
2025-10-06 12:58:17 +0200

    Add simple tests

---
+++ test/test.py
@@ -0,0 +1,24 @@
+from cap_sc_client import CapClient
+
+
+def test_dataset_search():
+    cp = CapClient()
+    df = cp.search_datasets(offset=5, limit=5)
+    assert df.shape[0] == 5
+
+def test_label_search():
+    cp = CapClient()
+    df = cp.search_cell_labels(offset=5, limit=5)
+    assert df.shape[0] == 5
+
+def test_md_session():
+    cp = CapClient()
+    datasets = cp.search_datasets(limit=1)
+    dataset_id = datasets["id"].to_list()[0]
+    md_session = cp.md_session(dataset_id=dataset_id)
+    md_session.create_session()
+    assert md_session.session_id is not None
+    assert len(md_session.embeddings) > 0
+    for emb_name in md_session.embeddings:
+        emb_data = md_session.embedding_data(embedding=emb_name, max_points=100)
+        assert emb_data is not None
